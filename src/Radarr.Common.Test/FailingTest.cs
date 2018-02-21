d3b412d385e2a3b1f89941ebe018b2431cd70eb1
Mike <aeonlucid@outlook.com>
2018-02-21 20:31:49 +0100

    Added more tests.

---
+++ src/Radarr.Common.Test/FailingTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Radarr.Common.Test
+{
+    public class FailingTest
+    {
+        [Test]
+        public void failing_test_example()
+        {
+            true.Should().BeFalse();
+        }
+    }
+}
